30163cc3ddd4662e105d0d527abc9dca2c6013e8
Daniel Gomez Beltran <danigb92trig@gmail.com>
2022-03-17 13:06:50 +0100

    Mostrar Imágenes

--- listuser.php
+++ listuser.php
@@ -6,9 +6,9 @@ $mensajeResultado = "";
 // Primero hacemos la consulta
 try {
 	$sql = "SELECT * FROM usuarios";
-	// Ejecutamos (conexion es el nombre de la conexión a la base de datos)
+// Ejecutamos (conexion es el nombre de la conexión a la base de datos)
 	$resultsquery = $conexion->query($sql);
-	//Revisamos OJO Preguntar por qué query
+//Revisamos OJO Preguntar por qué query
 	if ($resultsquery) {
 		$mensajeResultado = '<div class="alert alert-success">' .
 				"La consulta se realizó correctamente." . '</div>';
@@ -22,7 +22,7 @@ try {
 <?php require 'includes/header.php' ?>
 
 <body class="cuerpo">
-	<div class="container centrar" texA>
+	<div class="container centrar">
 		<div class="container cuerpo text-left">
 			<p><h2><img class="alineadoTextoImagen" src= "images/user.png"width="50px"/>
 				Base de Datos de Usuarios</h2></p>
@@ -33,16 +33,28 @@ try {
 				<th>Nombre</th>
 				<th>Apellidos</th>
 				<th>Email</th>
+				<th>Imagen</th>
+				<th>Operaciones</th>
 			</tr>
-			<?php
-			while ($fila = $resultsquery->fetch()) {
-				echo'<tr>';
-				echo'<td>' . $fila['nombre'] . '</td>';
-				echo'<td>' . $fila['apellidos'] . '</td>';
-				echo'<td>' . $fila['email'] . '</td>';
-				echo'</tr>';
-			}
-			?>
+			<?php while ($fila = $resultsquery->fetch()) { ?>
+
+				<tr>
+					<td><?= $fila["nombre"] ?> </td>
+					<td><?= $fila["apellidos"] ?> </td>
+					<td><?= $fila["email"] ?> </td>			
+					<td><?php
+						if ($fila["image"] != null) {
+							echo '<img src="uploads/'.$fila["image"].'" width="60" />'. $fila['image'];
+						}
+						?>
+					</td>
+					<td>
+						<a href="ver.php?idusuario=<?= $fila['idusuario'] ?>" class="btn btn-success">Ver</a>
+						<a href="edit.php?idusuario=<?= $fila['idusuario'] ?>" class="btn btn-warning">Editar</a>
+						<a href="delete.php?idusuario=<?= $fila['idusuario'] ?>" class="btn btn-danger">Eliminar</a>
+					</td>
+				</tr>
+<?php } ?>
 		</table>
 	</div>
 </body>
